d19e8471385897b9585303faea862101daf0a7cb
eliroy <eliroy@qq.com>
2017-05-09 16:29:20 +0800

    composer组件上传
    第一次上传

---
+++ src/Json.php
@@ -0,0 +1,14 @@
+<?php
+namespace eliroytest;
+class Json 
+{
+	public static function encode($data)
+    {
+		return json_encode($data);
+    }
+
+    public static function decode($jsonData)
+    {
+        return json_decode($jsonData, true);
+    } 
+}
